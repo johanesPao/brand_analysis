99e4855e4cc99ad77d68b70f4c618defb99165fa
Johanes Gregorius Indra Pradana Pao <me@johanespao.dev>
2024-12-20 18:50:34 +0700

    Add convert_int_to_month_name() and WIP use_existing_or_interpolate()

--- funcs.py
+++ funcs.py
@@ -1,3 +1,15 @@
+import calendar
+from datetime import datetime as dt
+
+def convert_int_to_month_name(month_int: int) -> str:
+    return calendar.month_name[month_int]
+
+def use_existing_or_interpolate(df_row):
+    if dt.now().month == df_row.month:
+        return "hooray"
+    else:
+        return "mehhh"
+
 def in_sql_concatenation(values: str | int | list[str] | list[int]):
     match values:
         case list() as lst:
