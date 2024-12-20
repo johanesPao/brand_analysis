00cf38ecdd58dbf5a5cbd570baccabe46e6fbc4d
Johanes Gregorius Indra Pradana Pao <me@johanespao.dev>
2024-12-20 18:48:19 +0700

    Adding plotly module and change bar_sales_by_month() to use plotly

--- plots.py
+++ plots.py
@@ -1,34 +1,56 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+from funcs import convert_int_to_month_name
 
 class Plots:
     def __init__(self):
         pass
 
     def bar_sales_by_month(self, brand = str, year = int, data = pd.DataFrame):
-        # Data Transformation
-        data = data[['month','cost','retail_tag','customer_paid']].groupby(['month'], as_index=False).sum()
-        data.rename(columns={"month":"Month", "cost": "COGS", "retail_tag": "Sales at Tag Price", "customer_paid": "Customer Paid"}, inplace=True)
-        sns_data = data.melt(id_vars=['Month'], var_name="Metrics", value_name="Amount (in Billion Rupiah)")
+        # Data selection and grouping by month
+        data = data[
+            [
+                'month',
+                'cost',
+                'retail_tag',
+                'customer_paid'
+            ]
+        ].groupby(['month'], as_index=False).sum()
+        data["month"] = data['month'].apply(lambda x: convert_int_to_month_name(x))
 
-        # Plot properties
-        sns.set_theme(palette="Paired")
-        fig, ax = plt.subplots()
-        fig.set_size_inches(15,5)
+        # Rename columns for display
+        data.rename(columns={
+            "month":"Month", 
+            "cost": "COGS", 
+            "retail_tag": "Sales at Tag Price", 
+            "customer_paid": "Customer Paid"
+        }, inplace=True)
 
-        # Plotting
-        barplot = sns.barplot(sns_data, x="Month", y="Amount (in Billion Rupiah)", hue="Metrics", ax=ax)
+        # Plot line for COGS, Tag Price and Customer Paid
+        fig = px.line(
+            data, 
+            x="Month", 
+            y=["COGS", "Sales at Tag Price", "Customer Paid"], 
+            color_discrete_sequence=['#FF2E63','#252A34','#00ADB5'], 
+            title=f"{brand} Sales Metrics in {year}", 
+            line_shape="spline"
+        )
 
-        # Title
-        ax.set_title(f"{brand} Sales Metrics in {year}")
+        # Update traces in figures
+        fig.update_traces(
+            mode="markers+lines", 
+            hovertemplate='Rp %{y:,.0f}<extra></extra>'
+        )
+        fig.update_yaxes(title_text="Amount")
+        fig.update_layout(
+            legend_title_text="Metrics", 
+            hovermode="x", 
+            autosize=True
+        )
 
-        # Remove legend title
-        handles, labels = barplot.get_legend_handles_labels()
-        barplot.legend(handles=handles[0:], labels=labels[0:])
-
-        # Remove scientific notation in yaxis
-        ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: '{:,.2f}B'.format(x/10**9)))
-
-        plt.show()
+        # Show line plot
+        fig.show()
